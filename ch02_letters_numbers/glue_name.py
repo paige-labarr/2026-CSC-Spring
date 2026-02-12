0628b5c99737141e66ee73f24299b725a21924b2
Paige LaBarr <paige.labarr@flcc.edu>
2026-02-12 13:53:21 +0000

    updates

---
+++ ch02_letters_numbers/glue_name.py
@@ -0,0 +1,79 @@
+# FILE NAME - glue_name.py
+
+# NAME: Paige LaBarr
+# DATE: February 4, 2026
+# BRIEF DESCRIPTION:  Join two strings together using concatenation (+) to "glue" two names together
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions below
+# 4. You can see the Sample Output below, too
+
+
+
+
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+    
+first = input("Enter first name: ") 
+last = input("Enter last name: ")
+print(f"Hi, {first} {last}")   
+    
+########### END YER CODE ABOVE THIS LINE ###########
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+Enter first name: Dave
+Enter last name: Ghidiu
+Hi, Dave Ghidiu
+'''
+
+
+
+'''
+Enter first name: bart
+Enter last name: simpson
+Hi, bart simpson
+'''
+
+
+
+'''
+Enter first name: Paige
+Enter last name: LaBarr
+Hi, Paige LaBarr
+'''
+
+
+
+'''
+Enter first name: Lisa
+Enter last name: Simpson, esquire
+Hi, Lisa Simpson, esquire
+'''
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. What is a lesson you learned during this lab? You do not have to restrict
+   your response to Python. It might be a lesson in time management, a lesson
+   using GitHub, or something else.
+
+I need to use git pull, git add, git commit, and git push when turning in work through GitHub Classroom.
+
+
+
+
+
+'''
